6ba7064b82bd64166fade11b1ff2eba85cbb7423
Erik <soer1776@student.ju.se>
2020-02-29 12:40:13 +0100

    add all question dependencies to container

--- web-application/src/presentation-layer/main.js
+++ web-application/src/presentation-layer/main.js
@@ -14,30 +14,27 @@ container.register('questionRouter', awilix.asFunction(questionRouter))
 // Business-logic-layer
 const accountManager = require('../business-logic-layer/account-manager')
 const accountValidator = require('../business-logic-layer/account-validator')
+const questionManager = require('../business-logic-layer/question-manager')
+const questionValidator = require('../business-logic-layer/question-validator')
 
 container.register("accountManager", awilix.asFunction(accountManager))
 container.register("accountValidator", awilix.asFunction(accountValidator))
-
+container.register("questionManager", awilix.asFunction(questionManager))
+container.register("questionValidator", awilix.asFunction(questionValidator))
 
 // Choose either Data-access-layer or Data-access-layer-sequelize
 
 // Data-access-layer 
-
 const Database = require('../data-access-layer/db')
-container.register("database", awilix.asClass(Database))
-
-
-
-
-
 const accountRepository = require('../data-access-layer/account-repository')
+const questionRepository = require('../data-access-layer/question-repository')
 
-//container.register("database", awilix.asClass(database))
+container.register("database", awilix.asClass(Database))
 container.register("accountRepository", awilix.asFunction(accountRepository))
+container.register("questionRepository", awilix.asFunction(questionRepository))
 
-
-// Data-access-layer-sequelize
 /*
+// Data-access-layer-sequelize
 const sequelize = require('../data-access-layer-sequelize/sequelize-setup')
 const accountRepository = require('../data-access-layer-sequelize/account-repository')
 
