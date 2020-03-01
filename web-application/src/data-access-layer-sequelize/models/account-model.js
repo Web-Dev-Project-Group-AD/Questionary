e7cd403cde3ebbd7a36e45b3080f250c0d585623
Erik <soer1776@student.ju.se>
2020-03-01 21:09:41 +0100

    update account-model

--- web-application/src/data-access-layer-sequelize/models/account-model.js
+++ web-application/src/data-access-layer-sequelize/models/account-model.js
@@ -1,13 +1,29 @@
+sequelize = require('sequelize')
+
 module.exports = sequelize.define('account', {
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    username: Sequelize.TEXT,
-    password: Sequelize.TEXT,
+    username: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
     isAdmin: {
         type: Sequelize.BOOLEAN,
         defaultValue: false
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
     }
 })
